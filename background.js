e3ecd57656c2c3adad0bad1f80291e5c300967cc
Joe Munoz <joe.rob.munoz@gmail.com>
2015-04-14 14:01:13 -0700

    Added stubbed data and mock ui.

--- background.js
+++ background.js
@@ -1,4 +1,25 @@
+'use strict';
+
 var linkBlockerApp = angular.module('LinkBlockerApp', []);
 
-linkBlockerApp.controller("backgroundCtrl", function($scope) {
+linkBlockerApp.controller('backgroundCtrl', function($scope) {
 });
+
+// Expose CRUD functions
+
+var linkService = (function () {
+
+	return {
+		getAll: function () {
+			console.log('Get all entities.');
+		},
+
+		insert: function (e) {
+			console.log('Insert: ' + e);
+		},
+
+		update: function (id) {
+			console.log('Attempted update: ' + id.toString());
+		}
+	};
+})();
--- popup.js
+++ popup.js
@@ -2,5 +2,19 @@ var linkBlockerApp = angular.module('LinkBlockerApp', []);
 
 linkBlockerApp.controller("PopUpCtrl", function($scope) {
   console.log("PopUpCtrl loaded");
-  $scope.foos = "Test";
+
+  // Stub data
+  $scope.linkEntities = [
+  	{
+  		disabled: true,
+  		link: "www.reddit.com"
+  	},
+  	{
+  		disabled: false,
+  		link: "www.angular.com"
+  	}
+  ];
+
+  // Load any models in
+  chrome.extension.getBackgroundPage().linkService.getAll();
 });
