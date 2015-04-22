5997cbc6221d64725b9080df427db036a3abc58b
Joe Munoz <joe.rob.munoz@gmail.com>
2015-04-22 12:27:53 -0700

    Updated basic CRUD for UI. No services yet.

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,53 @@
-var linkBlockerApp = angular.module('LinkBlockerApp', []);
+"use strict";
 
-linkBlockerApp.controller("PopUpCtrl", function($scope) {
-  console.log("PopUpCtrl loaded");
+(function () {
 
-  // Stub data
-  $scope.linkEntities = [
-  	{
-  		disabled: true,
-  		link: "www.reddit.com"
-  	},
-  	{
-  		disabled: false,
-  		link: "www.angular.com"
-  	}
-  ];
+	function PopUpCtrl ($scope) {
 
-  // Load any models in
-  chrome.extension.getBackgroundPage().linkService.getAll();
-});
+		// Stub data
+		$scope.linkEntities = [
+		  	{
+		  		id: 0,
+		  		disabled: true,
+		  		link: "www.reddit.com"
+		  	},
+		  	{
+		  		id: 1,
+		  		disabled: false,
+		  		link: "www.angular.com"
+		  	},
+		  	{
+		  		id: 2,
+		  		disabled: false,
+		  		link: "www.google.com"
+		  	}
+		];
+
+		$scope.create = function (url) {
+			// Stubbed Add till I make the service/resource.
+			if (url) {
+				$scope.linkEntities.push({
+					id: 4,
+					disabled: false,
+					link: url
+				})
+			};
+		};
+
+		// CRUD functions
+		$scope.delete = function (idx) {
+			// Call delete method from service
+			// chrome.extension.getBackgroundPage().linkService.delete(id);
+			// var objectBeingDeleted = $scope.linkEntities[idx];
+
+			// If successful, drop from view array. Splice is recommended.
+			$scope.linkEntities.splice(idx, 1);
+		};
+	};
+
+	angular.module('LinkBlockerApp', []);
+
+	angular
+		.module("LinkBlockerApp")
+		.controller("PopUpCtrl", PopUpCtrl);
+})();
